96cc7cb430b1fb8ac20d789e5c3dc64d6f735820
Justin Kaeser <github@accounts.justinkaeser.com>
2016-05-16 21:51:31 +0200

    this might work

--- build.sbt
+++ build.sbt
@@ -12,6 +12,8 @@ libraryDependencies ++= Seq(
   "org.rogach" %% "scallop" % "1.0.1" withSources(), // command line parser
   "com.typesafe.scala-logging" %% "scala-logging" % "3.4.0",
   "ch.qos.logback" % "logback-classic" % "1.1.7",
+
+  "org.scala-lang.modules" %% "scala-xml" % "1.0.5",
   "com.megatome.javadoc2dash" % "javadoc2dash-api" % "1.0.7",
   "org.eclipse.jgit" % "org.eclipse.jgit" % "4.3.1.201605051710-r"
 )
--- src/main/scala/org/gestern/Dashbukkit.scala
+++ src/main/scala/org/gestern/Dashbukkit.scala
@@ -3,15 +3,16 @@ package org.gestern
 import java.io.File
 
 import com.megatome.j2d.DocsetCreator
-import org.rogach.scallop._
 import com.typesafe.scalalogging.StrictLogging
 import org.eclipse.jgit.api.Git
+import org.eclipse.jgit.api.errors.EmtpyCommitException
 import org.eclipse.jgit.errors.RepositoryNotFoundException
 import org.eclipse.jgit.merge.MergeStrategy
 import org.eclipse.jgit.storage.file.FileRepositoryBuilder
+import org.rogach.scallop._
 
-import scala.collection.JavaConverters._
-import sys.process._
+import scala.sys.process._
+import scala.xml.{Elem, XML}
 
 
 object Dashbukkit extends App with StrictLogging {
@@ -22,18 +23,20 @@ object Dashbukkit extends App with StrictLogging {
     verify()
   }
 
-  val docsetName = "bukkit"
-
   logger.info("creating docset for bukkit")
 
+  val docsetName = "bukkit"
   val bukkitDir = new File("bukkit/")
   val targetDir = new File("target/")
+  val targetFile = new File(targetDir, s"$docsetName.docset")
+  val feedFile = new File(s"feed/$docsetName.xml")
+
 
   targetDir.mkdir()
 
   val remote = "https://hub.spigotmc.org/stash/scm/spigot/bukkit.git"
 
-  val git =
+  val gitBukkit =
     try {
       val repoBuilder = new FileRepositoryBuilder()
       val repo = repoBuilder.setMustExist(true).setWorkTree(bukkitDir).build()
@@ -43,17 +46,51 @@ object Dashbukkit extends App with StrictLogging {
         Git.cloneRepository().setURI(remote).setDirectory(bukkitDir).call()
     }
 
-  val pullResult = git.pull.setStrategy(MergeStrategy.THEIRS).call()
+  val gitThis = {
+    val repoBuilder = new FileRepositoryBuilder()
+    val repo = repoBuilder.setMustExist(true).setWorkTree(new File(".")).build()
+    new Git(repo)
+  }
+
+  val pullResult = gitBukkit.pull.setStrategy(MergeStrategy.THEIRS).call()
   if (pullResult.isSuccessful) {
     val createdJavaDoc = Process("mvn javadoc:javadoc", bukkitDir).!
     if (createdJavaDoc == 0) {
       new DocsetCreator.Builder(docsetName, new File(bukkitDir, "target/site/apidocs"))
-        .outputDirectory(targetDir).build()
+        .outputDirectory(targetDir)
+        .build()
         .makeDocset()
+
+      val pom = XML.loadFile(new File(bukkitDir, "pom.xml"))
+      val version = (pom \\ "project" \ "version").head.text
+
+      val targetTgzFile = targetTgz(version)
+      val tar = s"tar --exclude='.DS_Store' -czf ${targetTgzFile.getAbsolutePath} ${targetFile.getAbsolutePath}".!
+      assert(tar == 0 && targetTgzFile.isFile)
+
+      val feedData = feedXml(version)
+      XML.save(feedFile.getAbsolutePath, feedData)
+      gitThis.add().setUpdate(true).addFilepattern("feed/").call()
+
+      try {
+        gitThis.commit().setAllowEmpty(false).setMessage(s"update feed for $version").call()
+      } catch {
+        case _: EmtpyCommitException => // ignore
+      }
+      gitThis.tag().setForceUpdate(true).setName(s"v$version").call()
 
     } else sys.error("error creating javadoc")
 
   }
   else sys.error("pull failed")
+
+  def targetTgz(version: String) = new File(targetDir, s"$docsetName-$version.tgz")
+
+  def feedXml(version: String): Elem = {
+    <entry>
+      <version>{version}</version>
+      <url>https://github.com/jastice/dash-bukkit/archive/bukkit-{version}.tgz</url>
+    </entry>
+  }
 
 }
