a5c50a4abae37bd1d7964458fcf9bd30b723d168
Justin Kaeser <github@accounts.justinkaeser.com>
2016-05-16 17:36:11 +0200

    create bukkit docset from source

--- build.sbt
+++ build.sbt
@@ -11,7 +11,9 @@ libraryDependencies ++= Seq(
   "org.scalacheck" %% "scalacheck" % "1.12.5" % "test" withSources() withJavadoc(),
   "org.rogach" %% "scallop" % "1.0.1" withSources(), // command line parser
   "com.typesafe.scala-logging" %% "scala-logging" % "3.4.0",
-  "ch.qos.logback" % "logback-classic" % "1.1.7"
+  "ch.qos.logback" % "logback-classic" % "1.1.7",
+  "com.megatome.javadoc2dash" % "javadoc2dash-api" % "1.0.7",
+  "org.eclipse.jgit" % "org.eclipse.jgit" % "4.3.1.201605051710-r"
 )
 
 initialCommands := "import org.gestern.dashbukkit._"
--- src/main/scala/org/gestern/Dashbukkit.scala
+++ src/main/scala/org/gestern/Dashbukkit.scala
@@ -1,7 +1,18 @@
 package org.gestern
 
+import java.io.File
+
+import com.megatome.j2d.DocsetCreator
 import org.rogach.scallop._
 import com.typesafe.scalalogging.StrictLogging
+import org.eclipse.jgit.api.Git
+import org.eclipse.jgit.errors.RepositoryNotFoundException
+import org.eclipse.jgit.merge.MergeStrategy
+import org.eclipse.jgit.storage.file.FileRepositoryBuilder
+
+import scala.collection.JavaConverters._
+import sys.process._
+
 
 object Dashbukkit extends App with StrictLogging {
 
@@ -11,7 +22,38 @@ object Dashbukkit extends App with StrictLogging {
     verify()
   }
 
-  logger.info(s"The parameter was: ${Args.param()}, trailing args: ${Args.trailing()}")
-  println("""dash-bukkit awaits your orders. Usage:""")
-  Args.printHelp
+  val docsetName = "bukkit"
+
+  logger.info("creating docset for bukkit")
+
+  val bukkitDir = new File("bukkit/")
+  val targetDir = new File("target/")
+
+  targetDir.mkdir()
+
+  val remote = "https://hub.spigotmc.org/stash/scm/spigot/bukkit.git"
+
+  val git =
+    try {
+      val repoBuilder = new FileRepositoryBuilder()
+      val repo = repoBuilder.setMustExist(true).setWorkTree(bukkitDir).build()
+      new Git(repo)
+    } catch {
+      case notFound: RepositoryNotFoundException =>
+        Git.cloneRepository().setURI(remote).setDirectory(bukkitDir).call()
+    }
+
+  val pullResult = git.pull.setStrategy(MergeStrategy.THEIRS).call()
+  if (pullResult.isSuccessful) {
+    val createdJavaDoc = Process("mvn javadoc:javadoc", bukkitDir).!
+    if (createdJavaDoc == 0) {
+      new DocsetCreator.Builder(docsetName, new File(bukkitDir, "target/site/apidocs"))
+        .outputDirectory(targetDir).build()
+        .makeDocset()
+
+    } else sys.error("error creating javadoc")
+
+  }
+  else sys.error("pull failed")
+
 }
