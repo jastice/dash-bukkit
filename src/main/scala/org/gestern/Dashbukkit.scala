eeba892e5e4239edc35dccdbf7d3648560249423
Justin Kaeser <github@accounts.justinkaeser.com>
2016-05-18 13:30:31 +0200

    make things work
    maybe

--- src/main/scala/org/gestern/Dashbukkit.scala
+++ src/main/scala/org/gestern/Dashbukkit.scala
@@ -1,6 +1,8 @@
 package org.gestern
 
-import java.io.File
+import java.io._
+import java.nio.file.{Files, Paths}
+import java.security.{DigestInputStream, MessageDigest}
 
 import com.megatome.j2d.DocsetCreator
 import com.typesafe.scalalogging.StrictLogging
@@ -10,54 +12,47 @@ import org.eclipse.jgit.errors.RepositoryNotFoundException
 import org.eclipse.jgit.merge.MergeStrategy
 import org.eclipse.jgit.storage.file.FileRepositoryBuilder
 import org.eclipse.jgit.transport.UsernamePasswordCredentialsProvider
-import org.rogach.scallop._
 
-import scala.collection.JavaConverters._
 import scala.sys.process._
 import scala.xml.{Elem, XML}
 
 
-object Dashbukkit extends App with StrictLogging {
+object Dashbukkit extends StrictLogging {
 
-  object Args extends ScallopConf(args) {
-    val param = opt[String]("param", default=Some("default"), descr="This is an example parameter")
-    val trailing = trailArg[List[String]]("trailing", required=false, default=Some(Nil), descr="An optional trailing argument.")
-    verify()
-  }
+  def main(args: Array[String]) {
 
-  logger.info("creating docset for bukkit")
+    logger.info("creating docset for bukkit")
 
-  val docsetName = "bukkit"
-  val bukkitDir = new File("bukkit/")
-  val targetDir = new File("target/")
-  val targetFile = new File(targetDir, s"$docsetName.docset")
-  val feedFile = new File(s"feed/$docsetName.xml")
+    val docsetName = "bukkit"
 
+    val targetDir = new File("target/")
+    val targetFile = new File(targetDir, s"$docsetName.docset")
 
-  targetDir.mkdir()
+    val githubToken = sys.env("GITHUB_TOKEN")
+    val credentialsProvider = new UsernamePasswordCredentialsProvider(githubToken, "")
 
-  val remote = "https://hub.spigotmc.org/stash/scm/spigot/bukkit.git"
+    val feedRepo = "https://github.com/jastice/dash-bukkit-feed.git"
+    val feedDir = new File("dash-bukkit-feed/")
+    val feedFile = new File(feedDir, s"$docsetName.xml")
+    val feedHashFile = new File(feedDir, ".hash")
+    val feedGit = cloneOrUpdate(feedRepo, feedDir, credentialsProvider)
 
-  val gitBukkit =
-    try {
-      val repoBuilder = new FileRepositoryBuilder()
-      val repo = repoBuilder.setMustExist(true).setWorkTree(bukkitDir).build()
-      new Git(repo)
-    } catch {
-      case notFound: RepositoryNotFoundException =>
-        Git.cloneRepository().setURI(remote).setDirectory(bukkitDir).call()
-    }
+    val bukkitRepo = "https://hub.spigotmc.org/stash/scm/spigot/bukkit.git"
+    val bukkitDir = new File("bukkit/")
+    val bukkitGit = cloneOrUpdate(bukkitRepo, bukkitDir, credentialsProvider)
 
-  val gitThis = {
-    val repoBuilder = new FileRepositoryBuilder()
-    val repo = repoBuilder.setMustExist(true).setWorkTree(new File(".")).build()
-    new Git(repo)
-  }
+    targetDir.mkdir()
 
-  val pullResult = gitBukkit.pull.setStrategy(MergeStrategy.THEIRS).call()
-  if (pullResult.isSuccessful) {
-    val createdJavaDoc = Process("mvn javadoc:javadoc", bukkitDir).!
-    if (createdJavaDoc == 0) {
+    val bukkitPull = bukkitGit.pull.setStrategy(MergeStrategy.THEIRS).call()
+    val feedPull = feedGit.pull.setStrategy(MergeStrategy.THEIRS).call()
+
+    // something monad-y would be nicer here
+    val createdJavaDoc =
+      bukkitPull.isSuccessful &&
+        feedPull.isSuccessful &&
+        Process("mvn javadoc:javadoc", bukkitDir).! == 0
+
+    if (createdJavaDoc) {
       new DocsetCreator.Builder(docsetName, new File(bukkitDir, "target/site/apidocs"))
         .outputDirectory(targetDir)
         .build()
@@ -70,33 +65,47 @@ object Dashbukkit extends App with StrictLogging {
       val tar = s"tar --exclude='.DS_Store' -czf ${targetTgzFile.getAbsolutePath} ${targetFile.getAbsolutePath}".!
       assert(tar == 0 && targetTgzFile.isFile)
 
+      // hash to make sure an update commit is created iff the release file changes
+      val releaseHash = bukkitGit.log().setMaxCount(1).call().iterator().next().getName
+      val fw = new FileWriter(feedHashFile, false)
+      fw.write(releaseHash)
+      fw.close()
+
       val feedData = feedXml(version)
       XML.save(feedFile.getAbsolutePath, feedData)
-      gitThis.add().setUpdate(true).addFilepattern("feed/").call()
-      gitThis.tag().setForceUpdate(true).setName(s"v$version").call()
+      feedGit.add().setUpdate(true).addFilepattern(".").call()
+      feedGit.tag().setForceUpdate(true).setName(s"v$version").call()
       try {
-        gitThis.commit().setAllowEmpty(false).setMessage(s"update feed for $version").call()
+        feedGit.commit().setAllowEmpty(false).setMessage(s"update feed for $version").call()
       } catch {
         case _: EmtpyCommitException => // ignore
       }
+      feedGit.push().setCredentialsProvider(credentialsProvider).call()
+      feedGit.push().setCredentialsProvider(credentialsProvider).setPushTags().call()
 
-      val githubToken = sys.env("GITHUB_TOKEN")
-      val credentials = new UsernamePasswordCredentialsProvider(githubToken, "")
-      gitThis.push().setPushTags().setRemote("https://github.com/jastice/dash-bukkit.git").setCredentialsProvider(credentials).call()
+    } else sys.error("error pulling or creating javadoc or something")
 
-    }
-    else sys.error("error creating javadoc")
+    def targetTgz(version: String) = new File(targetDir, s"$docsetName-$version.tgz")
 
   }
-  else sys.error("pull failed")
 
-  def targetTgz(version: String) = new File(targetDir, s"$docsetName-$version.tgz")
 
   def feedXml(version: String): Elem = {
     <entry>
       <version>{version}</version>
       <url>https://github.com/jastice/dash-bukkit/archive/bukkit-{version}.tgz</url>
     </entry>
+  }
+
+  def cloneOrUpdate(remote: String, local: File, credentialsProvider: UsernamePasswordCredentialsProvider) = {
+    try {
+      val repoBuilder = new FileRepositoryBuilder()
+      val repo = repoBuilder.setMustExist(true).setWorkTree(local).build()
+      new Git(repo)
+    } catch {
+      case notFound: RepositoryNotFoundException =>
+        Git.cloneRepository().setURI(remote).setDirectory(local).setCredentialsProvider(credentialsProvider).call()
+    }
   }
 
 }
